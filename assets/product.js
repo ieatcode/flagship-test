d74848857bec748a6a11c13e454a95c7639977d5
Francis <developer@francismedia.net>
2023-03-05 21:23:24 +0800

    fr > show  product and cart info modal on product page load
    When the user accesses the product page, a Popup or Modal dialog should appear. (Use whichever library you want to).
    The modal should show how much stock does the current product have.
    The modal dialog should show information about the current user cart (which items are already in the cart, the “cart_index” of each item (developed in task A), how many, total amount in the cart)

--- assets/product.js
+++ assets/product.js
@@ -1,47 +1,61 @@
 class ProductDetails extends HTMLElement {
   constructor() {
-    super()
-    this.addToCartEl = document.getElementById('AddToCart');
-    this.addToCart = this.addToCart.bind(this)
-    this.init()
+    super();
+    this.addToCartEl = document.getElementById("AddToCart");
+    this.addToCart = this.addToCart.bind(this);
+    this.init();
   }
 
-  getCartItems = () => {
-    return fetch(`${routes.cart_url}`, {...fetchConfig(), method: 'GET'})
-      .then(response => response.json())
-      .then(state => {
-        return state
+  getCartItems = async () =>
+    fetch(`${routes.cart_url}`, { ...fetchConfig(), method: "GET" })
+      .then((response) => response.json())
+      .then((state) => {
+        return state;
       })
-      .catch((error) => console.log({error})
-      )
-  }
+      .catch((error) => console.log({ error }));
 
   addToCart = async () => {
-    const cartItems = await this.getCartItems()
+    const cartItems = await this.getCartItems();
     if (cartItems) {
-      const id = parseInt(document.getElementById('productSelect').value, 10)
-      const quantity = parseInt(document.getElementById('Quantity').value, 10)
-      const properties = {cart_index: cartItems?.items?.length}
-      const formPayload = {id, quantity, properties}
+      const id = parseInt(document.getElementById("productSelect").value, 10);
+      const quantity = parseInt(document.getElementById("Quantity").value, 10);
+      const properties = { cart_index: cartItems?.items?.length };
+      const formPayload = { id, quantity, properties };
       const body = JSON.stringify(formPayload);
-      fetch(`${routes.cart_add_url}`, {...fetchConfig(), ...{body}})
+      fetch(`${routes.cart_add_url}`, { ...fetchConfig(), ...{ body } })
         .then((response) => response.text())
         .then((state) => {
-          const payload = JSON.parse(state)
-        }).catch((error) => console.log({error}))
+          Swal.fire(
+            "Congratulations!",
+            "Item has been added to cart",
+            "success"
+          );
+        })
+        .catch((error) => console.log({ error }));
     }
-  }
+  };
 
   init() {
-    this.addToCartEl.addEventListener('click', async (event) => {
-      event.preventDefault()
-      await this.addToCart()
-    })
+    this.addToCartEl.addEventListener("click", async (event) => {
+      event.preventDefault();
+      await this.addToCart();
+    });
+    this.showQuantityModal();
   }
 
+  showQuantityModal() {
+    const product_details = document.querySelector(".product-details");
+
+    Swal.fire({
+      title: `Product Information`,
+      html: product_details,
+      icon: "info",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Got it!",
+    });
+  }
 }
 
-
-if (!customElements.get('product-details')) {
-  customElements.define('product-details', ProductDetails)
+if (!customElements.get("product-details")) {
+  customElements.define("product-details", ProductDetails);
 }
