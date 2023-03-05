d4dd0dda603fd8c1480710c5386014927075fe97
Francis dela Vega <ieatcode@users.noreply.github.com>
2023-03-05 15:13:14 +0800

    fix > move order notes to cart page (#3) (#4)

--- assets/product.js
+++ assets/product.js
@@ -2,10 +2,6 @@ class ProductDetails extends HTMLElement {
   constructor() {
     super()
     this.addToCartEl = document.getElementById('AddToCart');
-    this.toggleNotesEl = document.getElementById('add-instructions');
-    this.notesInputEl = document.getElementById('order-notes');
-    this.notesWrapEl = document.querySelector('.order-notes');
-    this.orderNote = ''
     this.addToCart = this.addToCart.bind(this)
     this.init()
   }
@@ -14,8 +10,6 @@ class ProductDetails extends HTMLElement {
     return fetch(`${routes.cart_url}`, {...fetchConfig(), method: 'GET'})
       .then(response => response.json())
       .then(state => {
-        this.orderNote = state.note
-        console.log('received items from cart')
         return state
       })
       .catch((error) => console.log({error})
@@ -28,10 +22,7 @@ class ProductDetails extends HTMLElement {
       const id = parseInt(document.getElementById('productSelect').value, 10)
       const quantity = parseInt(document.getElementById('Quantity').value, 10)
       const properties = {cart_index: cartItems?.items?.length}
-      const note = `${this.orderNote}
-        ${id}: ${this.notesInputEl.value}
-        `
-      const formPayload = {id, quantity, properties, note}
+      const formPayload = {id, quantity, properties}
       const body = JSON.stringify(formPayload);
       fetch(`${routes.cart_add_url}`, {...fetchConfig(), ...{body}})
         .then((response) => response.text())
@@ -41,21 +32,10 @@ class ProductDetails extends HTMLElement {
     }
   }
 
-  toggleNotes = () => {
-    if (this.toggleNotesEl.checked) {
-      this.notesWrapEl.style = "display: grid"
-    } else {
-      this.notesWrapEl.style = "display: none"
-    }
-  }
-
   init() {
     this.addToCartEl.addEventListener('click', async (event) => {
       event.preventDefault()
       await this.addToCart()
-    })
-    this.toggleNotesEl.addEventListener('change', (event) => {
-      this.toggleNotes()
     })
   }
 
